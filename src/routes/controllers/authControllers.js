3548ae513899fb888596dab800e807fe45bfaba4
Pedro Henrique Ribeiro de Azevedo <62411379+PedroHy@users.noreply.github.com>
2023-07-18 09:48:04 -0300

    auth error handling

--- src/routes/controllers/authControllers.js
+++ src/routes/controllers/authControllers.js
@@ -1,9 +1,16 @@
 const User = require('../../models/User');
 
 const register = async(req, res) => {
+
+    const { email } = req.body;
+
     try{
+        if(await User.findOne({email})){
+            return res.status(400).send(`Error: User already Exists`);
+        }
         const user = await User.create(req.body);
-        return res.send(user)
+        user.password = undefined;
+        return res.send('Sucessfully: User Registered')
     }catch(err){
         res.status(400).send(`Error: Registration Failed`);
     }
