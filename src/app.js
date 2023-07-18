8b603b90bdb821d525793aadece16bfc1e96cabb
Pedro Henrique Ribeiro de Azevedo <62411379+PedroHy@users.noreply.github.com>
2023-07-18 14:14:47 -0300

    creating test router

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 //routes
-const router = require('./routes/authRoute');
+const authRouter = require('./routes/authRoute');
+const profileRouter = require('./routes/profile');
 
 //DB connection
 mongoose.connect(`mongodb://127.0.0.1/${process.env.DB_NAME}`).then(()=>{
@@ -20,7 +21,8 @@ const app = express();
 
 app.use('/', bodyParser.json());
 app.use('/', bodyParser.urlencoded({extended: false}));
-app.use('/auth', router);
+app.use('/auth', authRouter);
+app.use('/profile', profileRouter);
 
 //server
 app.listen(process.env.PORT, ()=>{
