c597f291ed151c9d635a155117cfbd76b92ac084
Pedro Henrique Ribeiro de Azevedo <62411379+PedroHy@users.noreply.github.com>
2023-07-18 09:15:56 -0300

    connecting DB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect(`mongodb://127.0.0.1/${process.env.DB_NAME}`).then(()=>{
+    console.log('Sucessfully: DB connected');
+}).catch(()=>{
+    console.log('Error: DB was not connected');
+});
 
 const app = express();
 
@@ -8,5 +15,5 @@ app.use('/', bodyParser.json());
 app.use('/', bodyParser.urlencoded({extended: false}));
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`Server open on PORT ${process.env.PORT}`);
+    console.log(`Sucessfully: Server open on PORT ${process.env.PORT}`);
 })
